f03d84f16668a64c8a4e7f4edde79eb89f666908
wellifabio <robsonbacchindesouza@gmail.com>
2024-04-04 09:43:22 -0300

    primeiro commit

---
+++ api/src/controllers/veiculo.js
@@ -0,0 +1,9 @@
+const con = require('../connect/mysql');
+
+const readVeiculos = (req, res) => {
+    res.json("Read em construção");
+}
+
+module.exports = {
+    readVeiculos
+}
---
+++ api/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = express.Router();
+
+const Veiculo = require('./controllers/veiculo');
+
+routes.get('/', (req, res) => {
+    res.json("API Manutenções 1.0");
+});
+
+// routes.post('/usuarios/login', Usuario.login);
+// routes.post('/usuarios', Usuario.addUsuario);
+routes.get('/veiculos', Veiculo.readVeiculos);
+// routes.get('/usuarios/:id', Usuario.getUsuarios);
+// routes.put('/usuarios', Usuario.updateUsuario);
+// routes.delete('/usuarios/:id', Usuario.deleteUsuario);
+
+// routes.post('/tarefas', Tarefa.addTarefa);
+// routes.get('/tarefas', Tarefa.getTarefas);
+// routes.get('/tarefas/:id', Tarefa.getTarefas);
+// routes.get('/tarefas/status/:status', Tarefa.getTarefaStatus);
+// routes.get('/tarefas/data/:data', Tarefa.getTarefaData);    
+// routes.put('/tarefas', Tarefa.updateTarefa);
+// routes.delete('/tarefas/:id', Tarefa.deleteTarefa);
+
+module.exports = routes;
